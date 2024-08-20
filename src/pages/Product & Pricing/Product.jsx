11d3dfc34ce7946414bfe0152e320121bf208415
Mahdiyar733 <makhti733@gmail.com>
2024-08-20 12:14:20 +0330

    product

--- src/pages/Product & Pricing/Product.jsx
+++ src/pages/Product & Pricing/Product.jsx
@@ -13,8 +13,8 @@ export default function Product() {
 			{!isOpen ? (
 				<section className="h-dvh flex flex-col w-full md:flex-row items-center p-9 py-16 gap-5 md:px-1 lg:gap-16 md:gap-9">
 					<img
-						src="./img-1.jpg"
-						className="flex-[2] object-cover w-80 aspect-square rounded-lg sm:flex-1 max-w-[380px] animate-fade"
+						src="/img-1.jpg"
+						className="flex-[2] object-cover sm:max-h-auto max-h-[300px] rounded-lg sm:flex-1 max-w-[380px] animate-fade"
 						alt="person with dog overlooking mountain with sunset"
 					/>
 					<div className="flex-1 flex flex-col justify-center backdrop-saturate-200 p-2 md:p-0 animate-fade-right">
